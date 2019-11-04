dd3dc9892ade331533538f049865f0447a3b5b3c
Nethelbert <gilblasse@gmail.com>
2019-11-04 22:57:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word(sentence)
   sentence_array = []
-  sentence.each {|word| sentence_array << word.reverse}
+  sentence.each{|word| sentence_array << word.reverse}
   sentence_array.join(' ')
 end
